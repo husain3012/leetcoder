e592b9d93ab5fd87286987b48d12e85930f21e5f
Husain Shahid Rao <hsr3012kalimg@gmail.com>
2023-05-08 04:14:22 +0530

    add insights

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,10 @@
-
+import { Analytics } from "@vercel/analytics/react";
 const MyApp = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 };
 export default MyApp;
