83dd2d84a58866ec51365a622c069810b9490ded
Husain Shahid Rao <hsr3012kalimg@gmail.com>
2023-05-13 07:11:24 +0530

    remove scraping from main app

--- pages/api/cron.ts
+++
@@ -1,8 +0,0 @@
-// import { updateQueue } from "../../utils";
-
-const handler = async (req, res) => {
-    // const updatedUsers = await updateQueue(50, 60*30, 1100)
-  res.status(200).end("updatedUsers");
-};
-
-export default handler;
