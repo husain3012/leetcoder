910cdc45e7e150dff7a39add6878e9b80c53a7d7
Husain Shahid Rao <hsr3012kalimg@gmail.com>
2023-05-31 19:14:38 +0530

    idk what changed but now it works

--- pages/api/groups/join/[invite_id].ts
+++ pages/api/groups/join/[invite_id].ts
@@ -84,7 +84,7 @@ const postHandler = async (req: NextApiRequest, res: NextApiResponse) => {
   } catch (error) {
     res
       .status(error.status || 500)
-      .send(error.message || "Something went wrong");
+      .send(error.message || "Something went wrong, Try Again Later");
   }
 };
 
